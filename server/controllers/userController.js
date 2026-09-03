bf6b5421404d84e4ae7d989040e3021daf58b7db
Maniha-dev <nasirrazaptcl@gmail.com>
2026-09-03 23:20:35 +0500

    update payment method

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -159,10 +159,14 @@ export const isAuth = async (req, res) => {
 
         const user = await User.findById(userId).select("-password");
 
+        if (!user) {
+            return res.json({ success: false, message: 'user missing' });
+        }
+
         return res.json({ success: true, user });
     } catch (error) {
         console.log(error.message);
-        res.json({ success: false, message: error.message });
+        res.status(500).json({ success: false, message: error instanceof Error ? error.message: 'Internal server error' });
     }
 };
 
